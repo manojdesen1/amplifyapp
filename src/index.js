5f6973daf125b472a6a76437bc97643017ca1075
manojdesen1 <107014029+manojdesen1@users.noreply.github.com>
2023-07-08 14:18:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Amplify } from 'aws-amplify'; // Add this import statement
+import config from './aws-exports'; // Add this import statement
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(config); // Configure Amplify with the provided config object
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +15,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
